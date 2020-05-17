fda37c137eb41900999df6f8e096691b4b27a679
lowie-einbinder <lowieeinbinder@gmail.com>
2020-05-17 16:49:10 -0400

    Adding feature to create new product using HTML form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,27 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    # make a new product with what product_params returns (which is a method we're calling)
+    @product = Product.new(product_params)
+    if @product.save
+      # if the save for the product was successful, go to show.html.erb
+      redirect_to product_url(@product)
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'products' => 'products#index'
+
+  post 'products' => 'products#create' 
+  get 'products/new' => 'products#new'
+
   get 'products/:id' => 'products#show', as: 'product'
 end
-
